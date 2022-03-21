588375c8fae96a967397bb665a7d63bbcca8b27f
Niels J. de Winter <32540829+nielsjdewinter@users.noreply.github.com>
2022-03-21 14:47:53 +0100

    Plot aesthetic update
    Corrected axis labels and legend

--- 06_Full_aragonite_plot.r
+++ 06_Full_aragonite_plot.r
@@ -1,10 +1,13 @@
 require(ggplot2) # for plotting
 require(ggpubr) # for violin plots
-require(gridExtra) # for combining plots
 require(RColorBrewer) # to define color scales
 require(ggrepel) # for plot labeling
 
 # Plot full aragonite dataset with regressions ------------------------------------------------------------
+# Prepare secondary temperature axis
+temp_labs <- c("0", "5", rep("", 3), "25", rep("", 4), "50", rep("", 14), "125", rep("", 24), "250", rep("", 149), "1000")
+temp_labs_lowT <- c(as.character(seq(0, 30 ,5)), "", "40", "", "50", "", "60", "", "70", "", "80", "", "90", "", "100", rep("", 4), "125", rep("", 24), "250", rep("", 149), "1000")
+temp_breaks <- seq(0, 1000, 5)
 
 # Build custom color scales for full dataset plot
 colorscale <- unlist(brewer.pal(length(unique(dat$Analysis)), "Set1"))
@@ -16,22 +19,8 @@ uniquesamples <- data.frame(samplename = unique(violin_data$sample),
 fillscale <- colorscale[match(uniquesamples$Analysis, names(colorscale))]
 names(fillscale) <- uniquesamples$samplename
 
-
 Aragonite_D47_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) +
 # Regressions
-#    geom_ribbon(data = D47m_poly_MC_result,
-#        aes(x = x,
-#        y = fit,
-#        ymin = lwr,
-#        ymax = upr),
-#        fill = "lightblue",
-#        alpha = 0.3) +
-#    geom_line(data = D47m_poly_MC_result,
-#        aes(x = x, y = fit),
-#        color = "lightblue",
-#        linetype = "dashed",
-#        cex = 1,
-#        alpha = 1) +
     geom_ribbon(data = D47m_York_result,
         aes(x = x,
         y = fit,
@@ -61,13 +50,12 @@ Aragonite_D47_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) 
     geom_line(data = Anderson21,
         aes(x = Temp, y = D47),
         color = "black",
-        linetype = "dashed",
         cex = 1,
         alpha = 1) +
     geom_line(data = MeinickeICDES[1:1001, ],
         aes(x = Temp, y = D47),
         color = "black",
-        linetype = "dotted",
+        linetype = "dashed",
         cex = 1,
         alpha = 1) +
     geom_line(data = Guo09,
@@ -131,6 +119,7 @@ Aragonite_D47_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) 
         cex = 1,
         alpha = 1) +
 # Plot layout
+    ylim(0, max(D47stats$D47_mean + D47stats$CL95)) +
     scale_x_continuous(10 ^ 6 / T ^ 2 ~ "(K)",
         breaks = seq(0, 13.5, 1),
         minor_breaks = seq(0, 13.5, 0.25),
@@ -138,8 +127,9 @@ Aragonite_D47_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) 
             "Temperature (°C)",
             breaks = temp_breaks,
             labels = temp_labs)) +
-    ggtitle(expression("Aragonite "~Delta[47]~" vs Temperature")) +
-    coord_cartesian(ylim = c(0, max(D47stats$D47_mean + D47stats$CL95))) +
+    scale_y_continuous(Delta[47] ~ "(\u2030"~"I-CDES)",
+        breaks = seq(0, 0.8, 0.2)) +
+    ggtitle("A") +
     labs(x = 10 ^ 6 / T ^ 2 ~ "(K)",
         y = Delta * Delta[47] ~ "(\u2030"~"I-CDES)",
         colour = "Legend") +
@@ -148,20 +138,23 @@ Aragonite_D47_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) 
     scale_shape_manual(values = c(15:20)) +
     theme_bw()
 
+
 # Add zoom in for low temperatures----------------------------------------------
 Aragonite_D47_plot_lowT <- Aragonite_D47_plot +
-    coord_cartesian(ylim = c(0.3, 0.8)) +
+    ylim(0.3, 0.8) +
     scale_x_continuous(10 ^ 6 / T ^ 2 ~ "(K)",
-        breaks = seq(0, 13.5, 0.5),
-        minor_breaks = seq(0, 13.5, 0.25),
+        breaks = seq(0, 13.5, 1),
+        minor_breaks = seq(0, 13.5, 0.5),
         sec.axis = sec_axis(~ sqrt(1e6 / .) - 273.15,
             "Temperature (°C)",
             breaks = temp_breaks,
             labels = temp_labs_lowT),
         limits = c(7, 13.5)) +
-    ggtitle(expression("Aragonite "~Delta[47]~" vs Temperature")) +
-    theme(legend.position = "none")
+    ggtitle("B")
 
-combined_plots_Aragonite <- grid.arrange(Aragonite_D47_plot + theme(legend.position = "none"),
+combined_plots_Aragonite <- ggarrange(Aragonite_D47_plot,
     Aragonite_D47_plot_lowT,
-    ncol = 1)
+    ncol = 1,
+    common.legend = TRUE,
+    legend="bottom"
+)
