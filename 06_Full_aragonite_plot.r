6b40a7aa9656a43a8519f39a878c0e3c77628243
Niels J. de Winter <32540829+nielsjdewinter@users.noreply.github.com>
2022-05-06 15:47:22 +0200

    Pre-submission leaner plot

--- 06_Full_aragonite_plot.r
+++ 06_Full_aragonite_plot.r
@@ -1,13 +1,10 @@
 require(ggplot2) # for plotting
 require(ggpubr) # for violin plots
+require(gridExtra) # for combining plots
 require(RColorBrewer) # to define color scales
 require(ggrepel) # for plot labeling
 
 # Plot full aragonite dataset with regressions ------------------------------------------------------------
-# Prepare secondary temperature axis
-temp_labs <- c("0", "5", rep("", 3), "25", rep("", 4), "50", rep("", 14), "125", rep("", 24), "250", rep("", 149), "1000")
-temp_labs_lowT <- c(as.character(seq(0, 30 ,5)), "", "40", "", "50", "", "60", "", "70", "", "80", "", "90", "", "100", rep("", 4), "125", rep("", 24), "250", rep("", 149), "1000")
-temp_breaks <- seq(0, 1000, 5)
 
 # Build custom color scales for full dataset plot
 colorscale <- unlist(brewer.pal(length(unique(dat$Analysis)), "Set1"))
@@ -21,6 +18,19 @@ names(fillscale) <- uniquesamples$samplename
 
 Aragonite_D47_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) +
 # Regressions
+    geom_ribbon(data = D47m_poly_MC_result,
+        aes(x = x,
+        y = fit,
+        ymin = lwr,
+        ymax = upr),
+        fill = "lightblue",
+        alpha = 0.3) +
+    geom_line(data = D47m_poly_MC_result,
+        aes(x = x, y = fit),
+        color = "lightblue",
+        linetype = "dashed",
+        cex = 1,
+        alpha = 1) +
     geom_ribbon(data = D47m_York_result,
         aes(x = x,
         y = fit,
@@ -50,25 +60,27 @@ Aragonite_D47_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) 
     geom_line(data = Anderson21,
         aes(x = Temp, y = D47),
         color = "black",
+        linetype = "dashed",
         cex = 1,
         alpha = 1) +
     geom_line(data = MeinickeICDES[1:1001, ],
         aes(x = Temp, y = D47),
         color = "black",
-        linetype = "dashed",
+        linetype = "dotted",
         cex = 1,
         alpha = 1) +
-    geom_line(data = Guo09,
-        aes(x = Temp, y = D47_cc),
-        color = "grey",
-        cex = 1,
-        alpha = 1) +
-    geom_line(data = Guo09,
-        aes(x = Temp, y = D47_ar),
-        color = "grey",  
-        linetype = "dashed",
-        cex = 1,
-        alpha = 1) +
+#    geom_line(data = Guo09,
+#        aes(x = Temp, y = D47_cc),
+#        color = "blue",
+#        linetype = "dashed",
+#        cex = 1,
+#        alpha = 0.5) +
+#    geom_line(data = Guo09,
+#        aes(x = Temp, y = D47_ar),
+#        color = "darkgreen",
+#        linetype = "dashed",
+#        cex = 1,
+#        alpha = 0.5) +
 # Violin plots
     geom_violin(data = violin_data[which(!violin_data$outlier), ],
         aes(10^6 / (Temp + 273.15) ^ 2,
@@ -119,7 +131,6 @@ Aragonite_D47_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) 
         cex = 1,
         alpha = 1) +
 # Plot layout
-    ylim(0, max(D47stats$D47_mean + D47stats$CL95)) +
     scale_x_continuous(10 ^ 6 / T ^ 2 ~ "(K)",
         breaks = seq(0, 13.5, 1),
         minor_breaks = seq(0, 13.5, 0.25),
@@ -130,8 +141,9 @@ Aragonite_D47_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) 
     scale_y_continuous(Delta[47] ~ "(\u2030"~"I-CDES)",
         breaks = seq(0, 0.8, 0.2)) +
     ggtitle("A") +
+    coord_cartesian(ylim = c(0, max(D47stats$D47_mean + D47stats$CL95))) +
     labs(x = 10 ^ 6 / T ^ 2 ~ "(K)",
-        y = Delta * Delta[47] ~ "(\u2030"~"I-CDES)",
+        y = Delta[47] ~ "(\u2030"~"I-CDES)",
         colour = "Legend") +
     scale_colour_manual(values = colorscale) +
     scale_fill_manual(values = fillscale) +
@@ -141,16 +153,20 @@ Aragonite_D47_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) 
 
 # Add zoom in for low temperatures----------------------------------------------
 Aragonite_D47_plot_lowT <- Aragonite_D47_plot +
-    ylim(0.3, 0.8) +
+    coord_cartesian(ylim = c(0.55, 0.7)) +
     scale_x_continuous(10 ^ 6 / T ^ 2 ~ "(K)",
-        breaks = seq(0, 13.5, 1),
+        breaks = seq(0, 13, 1),
         minor_breaks = seq(0, 13.5, 0.5),
         sec.axis = sec_axis(~ sqrt(1e6 / .) - 273.15,
             "Temperature (°C)",
             breaks = temp_breaks,
             labels = temp_labs_lowT),
-        limits = c(7, 13.5)) +
+        limits = c(10 ^ 6 / (30 + 273.15) ^ 2, 13.5)) +
+    scale_y_continuous(Delta[47] ~ "(\u2030"~"I-CDES)",
+        breaks = seq(0.5, 0.8, 0.1)) +
     ggtitle("B")
+
+Aragonite_D47_plot_lowT <- remove_geom(Aragonite_D47_plot_lowT, "GeomViolin")
 
 combined_plots_Aragonite <- ggarrange(Aragonite_D47_plot,
     Aragonite_D47_plot_lowT,
