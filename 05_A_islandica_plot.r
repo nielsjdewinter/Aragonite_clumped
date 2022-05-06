5049734e17571d0eac0f4f9c1b336921d40319eb
Niels J. de Winter <32540829+nielsjdewinter@users.noreply.github.com>
2022-05-06 15:45:55 +0200

    Pre-submission lean plot

--- 05_A_islandica_plot.r
+++ 05_A_islandica_plot.r
@@ -17,60 +17,30 @@ Ais_colorscale <- unlist(brewer.pal(length(unique(Aisstats$sample)), "RdBu"))
 names(Ais_colorscale) <- unique(Aisstats$sample[order(Aisstats$Temp, decreasing = TRUE)])
 
 Ais_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) +
-# Anderson and Meinicke calibration and York regressions
+# Anderson, Meinicke and Guo regressions
     geom_line(data = Anderson21,
+        aes(x = Temp, y = D47),
+        color = "black",
+        cex = 1,
+        alpha = 1) +
+    geom_line(data = MeinickeICDES[1:301, ],
         aes(x = Temp, y = D47),
         color = "black",
         linetype = "dashed",
         cex = 1,
         alpha = 1) +
-    geom_line(data = MeinickeICDES[1:1001, ],
-        aes(x = Temp, y = D47),
-        color = "black",
-        linetype = "dotted",
+    geom_line(data = Guo09,
+        aes(x = Temp, y = D47_cc),
+        color = "grey",
         cex = 1,
         alpha = 1) +
-    geom_ribbon(data = Ais_York_result,
-        aes(x = x,
-        y = fit,
-        ymin = lwr,
-        ymax = upr),
-        fill = "grey",
-        alpha = 0.5) +
-    geom_ribbon(data = Mollusk_York_result,
-        aes(x = x,
-        y = fit,
-        ymin = lwr,
-        ymax = upr),
-        fill = "lightblue",
-        alpha = 0.5) +
-# Violin plots
-    geom_violin(data = violin_data[which(!violin_data$outlier & violin_data$Analysis == "this study"), ],
-        aes(10^6 / (Temp + 273.15) ^ 2,
-            y = D47,
-            fill = sample),
-            kernel = "rectangular",
-            scale = "area",
-            position = "identity",
-            width = .1,
-            cex = 0,
-            alpha = 0.1,
-            color = NA,
-            trim = FALSE,
-            show.legend = FALSE) +
+    geom_line(data = Guo09,
+        aes(x = Temp, y = D47_ar),
+        color = "grey",  
+        linetype = "dashed",
+        cex = 1,
+        alpha = 1) +
 # Measurements
-    geom_point(data = dat[which(!dat$D47_outlier & dat$Analysis == "this study"), ],
-        aes(x = 10^6 / (Temp + 273.15) ^ 2,
-            y = D47,
-            color = sample),
-        alpha = 0.2,
-        cex = 1) + 
-    geom_point(data = dat[which(dat$ID == "A.islandica"), ],
-        aes(x = 10^6 / (Temp + 273.15) ^ 2,
-            y = D47,
-            color = "Bernasconi18"),
-        alpha = 0.2,
-        cex = 1) + 
     geom_pointrange(data = Aisstats,
         aes(x = 10^6 / (Temp + 273.15) ^ 2,
             y = D47_mean,
@@ -107,7 +77,7 @@ Ais_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) +
         cex = 1,
         alpha = 1) +
 # Plot layout
-    scale_x_continuous(10^6/T^2~"(K)",
+    scale_x_continuous(10 ^ 6 / T ^ 2 ~ "(K)",
         breaks = seq(11, 14, 0.5),
         minor_breaks = seq(11, 14, 0.25),
         sec.axis = sec_axis(~ sqrt(1e6 / .) - 273.15,
@@ -118,8 +88,8 @@ Ais_plot <- ggplot(data = dat, aes(10^6 / (Temp + 273.15) ^ 2 , D47)) +
         ylim = c(0.6, 0.72)
     ) +
     ggtitle(expression("Arctica islandica "~Delta[47]~" vs Temperature")) +
-    labs(x = 10^6/T^2~"(K)",
-        y = Delta*Delta[47]~"(\u2030"~"I-CDES)",
+    labs(x = 10 ^ 6 / T ^ 2 ~ "(K)",
+        y = Delta[47] ~ "(\u2030"~"I-CDES)",
         colour = "Legend") +
     scale_colour_manual(values = Ais_colorscale) +
     scale_fill_manual(values = Ais_colorscale) +
